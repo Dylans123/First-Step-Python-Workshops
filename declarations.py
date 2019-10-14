bec47334afd4473f5981cf2fa7ed9015eb80bc22
dylans123 <dylanskelly@knights.ucf.edu>
2019-10-14 15:05:46 -0400

    Add explanation of declarations

--- declarations.py
+++ declarations.py
@@ -1,6 +1,33 @@
 """
 DECLARATIONS
 
+EXPLANATION
 When you declare something in a programming language, your basically assigning
 the value to the right of the equals sign to the symbol on the left of the equals sign.
+Below is an example of declaring a variable a equal to a value 5.
 """
+
+a = 5
+
+"""
+BASIC TYPES
+In programming Python, there are many different types that we can assign to a variable. These
+types represent things we see in everyday life, like numbers, words, truths, etc. The types that
+we will be concerned with are going to include all of the ones below. Above each is the type of value
+it represents and what its equivalent in the real world would be.
+"""
+
+#String represents words in real world
+a = "Hello"
+
+#Integer represents whole numbers in real world.
+b = 1
+
+#Float represents decimal values in real world.
+c = 1.1
+
+#None represents having nothing in the real world.
+d = None
+
+#Boolean represnts true of false in the real world.
+e = True
